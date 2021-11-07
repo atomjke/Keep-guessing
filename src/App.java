221de45c34284f97534c33271a0fdbd84dcfcb37
Evgeny Turmy <jenqa29@mail.ru>
2021-11-07 09:13:24 +0200

    Project added

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("I have chosen a number between 1 and 10. Try to guess it.");
+
+        int randomizedValue, inputValue;
+
+        randomizedValue = (int)(Math.random() * 10) + 1;
+
+        Scanner input = new Scanner(System.in);
+
+        inputValue = input.nextInt();
+
+        while (inputValue != randomizedValue) {
+            System.out.println("That is incorrect. Guess again.");
+            System.out.print("Your guess: ");
+
+            inputValue = input.nextInt();
+        }
+
+        if (inputValue == randomizedValue) {
+            System.out.println("That's right! You're a good guesser.");
+        }
+    }
+}
